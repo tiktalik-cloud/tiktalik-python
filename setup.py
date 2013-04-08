df5b19141cf5a338c7206173a1dc5fcd188519e5
Krzysztof Godlewski <krzysztof.godlewski@tiktalik.com>
2013-04-08 15:57:21 +0200

    Updated setup.py for PyPi upload

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
 setup(
-	name='tiktalik-python',
-	version='0.1',
-	description='Tiktalik Python API',
-	author='Tiktalik.com',
-	author_email='kontakt@tiktalik.com',
-	url='http://tiktalik.com',
+	name="tiktalik",
+	version="1.0",
+	description="Python SDK for the Tiktalik Computing service.",
+	author="Techstorage sp. z o.o",
+	author_email="kontakt@tiktalik.com",
+	url="http://www.tiktalik.com",
+	classifiers=[
+		"Programming Language :: Python",
+		"Topic :: Internet",
+		"Topic :: Software Development :: Libraries",
+		"License :: OSI Approved :: MIT License",
+	],
 
-	packages=['tiktalik', 'tiktalik.computing'],
+	packages=["tiktalik", "tiktalik.computing"],
 )
