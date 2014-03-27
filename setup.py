21e3fce674eb106ca16bc22fcda1c0077d9e5b0a
Adam Mazur <ghr@o2.pl>
2014-03-27 16:43:10 +0100

    version 1.4.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name="tiktalik",
-	version="1.4",
+	version="1.4.2",
 	description="Python SDK for the Tiktalik Computing service.",
 	author="Techstorage sp. z o.o",
 	author_email="kontakt@tiktalik.com",
